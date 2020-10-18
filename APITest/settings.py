4226ae31fded3badffe70a88eadee694aae9301a
shaurya172 <shauryavardhan1@gmail.com>
2020-10-18 23:15:03 +0530

    db sqlite

--- APITest/settings.py
+++ APITest/settings.py
@@ -78,25 +78,25 @@ WSGI_APPLICATION = 'APITest.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb',
-        'USER': 'user1',
-        'PASSWORD': 'pass1',
-        'HOST' : 'localhost',
-        'PORT' : '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mydb',
+#         'USER': 'user1',
+#         'PASSWORD': 'pass1',
+#         'HOST' : 'localhost',
+#         'PORT' : '5432',
+#
+#     }
+# }
 
 
 # Password validation
