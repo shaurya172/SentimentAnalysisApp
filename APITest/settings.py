94fb83007cd7903562ad745770b25774a3ee4795
shaurya172 <shauryavardhan1@gmail.com>
2020-10-19 14:55:21 +0530

    static changes

--- APITest/settings.py
+++ APITest/settings.py
@@ -147,7 +147,7 @@ REST_FRAMEWORK = {
     )
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
