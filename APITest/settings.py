ed07c875b9882c1f74892434026c694846279329
shaurya172 <shauryavardhan1@gmail.com>
2020-10-19 20:52:58 +0530

    added logging

--- APITest/settings.py
+++ APITest/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '0gs9+o=)holwfn@h1dbg)8+w31z02o612o7v$6hasn33!7^aok'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://sentiment-ml-app.herokuapp.com/']
+ALLOWED_HOSTS = ['https://sentiment-ml-app.herokuapp.com','localhost']
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -155,6 +155,8 @@ STATICFILES_DIRS = [
 ]
 
 django_heroku.settings(locals())
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGGING = {
     'version': 1,
