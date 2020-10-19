f9aaa8fdf1b6fec6b5d66a8489df32cd4057cd5e
shaurya172 <shauryavardhan1@gmail.com>
2020-10-19 11:52:25 +0530

    postgres config

--- APITest/settings.py
+++ APITest/settings.py
@@ -80,24 +80,24 @@ WSGI_APPLICATION = 'APITest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'mydb',
-#         'USER': 'user1',
-#         'PASSWORD': 'pass1',
-#         'HOST' : 'localhost',
-#         'PORT' : '5432',
-#
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DEMO_TEST',
+        'USER': 'postgres',
+        'PASSWORD': 'test1234',
+        'HOST' : 'localhost',
+        'PORT' : '5432'
+
+    }
+}
 
 
 # Password validation
--- ytube/serializers.py
+++ ytube/serializers.py
@@ -5,3 +5,4 @@ class sentimentSerializers(serializers.ModelSerializer):
 	class Meta:
 		model=sentiment
 		fields='__all__'
+		managed = True
